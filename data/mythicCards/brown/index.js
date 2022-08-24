ac472f724a4f50d2574ba58b4bc17c2eec49ea95
AlexSerSla <slavckin2009@yandex.ru>
2022-08-24 23:57:13 +0300

    feat: create no Webpack index.js files

--- data/mythicCards/brown/index.js
+++ data/mythicCards/brown/index.js
@@ -1,4 +1,4 @@
-import brownCardsAssets from '../../../assets/MythicCards/brown';
+import brownCardsAssets from '../../../assets/MythicCards/brown/indexNWP.js';
 
 const cardsData = [
   {
--- data/script.js
+++ data/script.js
@@ -1,7 +1,15 @@
 
-import greenCards from './difficulties';
+import difficulties from '../data/difficulties.js';
+import ancientsData from '../data/ancients.js'
+import blueCards from '../data/mythicCards/blue/index.js';
+import orangeCards from '../data/mythicCards/brown/index.js';
+import greenCards from '../data/mythicCards/green/index.js';
 
-console.log(greenCards);
+console.log(blueCards[0].cardFace);
+console.log(orangeCards[10].cardFace);
+console.log(greenCards[4].cardFace);
+console.log(difficulties[0].id);
+console.log(ancientsData[0].firstStage);
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
